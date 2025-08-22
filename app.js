6402d00bb47d9b2c2fccbc68e928b2cc51943439
syed-saad-raza <syedsaadfurqan10@gmail.com>
2025-08-22 10:48:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,153 @@
+// console.log("JavaScript Connected!");
+
+// Ch:Alert
+
+// Q#1
+// alert("Welcome to My Website!");
+
+// Q#2
+// alert("Erorr ! Please Enter A Valid Password:");
+
+// Q#3
+// alert("Welcome to JS Land \nHappy Coding");
+
+// Q#4
+// alert("Welcome to JS Land");
+// alert("Happy Coding");
+
+// Q#5
+// alert("Hello, I Can Run JS Through My Web Console!");
+// console.log("Hello, I Can Run JS Through My Web Console!");
+
+// Q#6
+// alert("Write On HTML Page!");
+// document.write("Write On HTML Page!")
+
+// Ch:Varibles For Strings
+
+// Q#7
+// var userName="";
+
+// Q#8
+// var Myname="Syed Saad Raza:";
+
+// Q#9
+// var message = "";
+// message = "Hello World !";
+// alert(message);
+
+// Q#10
+// alert("Syed Saad Raza:");
+// alert("15 years old:");
+// alert("Website Devlopment:");
+
+// Q#11
+// alert("Pizza \nPizz \nPiz \nPi \nP");
+
+// Q#12
+// var email="syedsaadfurqan10@gmail.com";
+// alert(email);
+
+// Q#13
+// var Books="A Smatter Way To Learn JavaScript !";
+// alert(Books);
+
+// Q#14
+// var JS ="Yah! I Can Write HTML Content through JS";
+// document.write(JS);
+
+// Q#15
+// var design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+// Ch:Varibles For Number
+
+// Q#16
+// var Age=16;
+// alert(Age);
+
+// Q#17
+// var visiter = 18;
+// alert("You Have Visited the Sites " + visiter + " times ");
+
+// Q#18
+// var Birthyear = 2010;
+// document.write("My Birth Year is  " +  Birthyear);
+
+// Q#19
+// var Name = "Syed Saad Raza";
+// var producttitle = "T.Shirt";
+// var quantity = 18;
+// var result = Name  + " " + producttitle  + " " +  quantity  +  " On SFA Fabrics ";
+// document.write(result);
+
+// Ch:Varible Legal & Illegal
+
+// Q#20
+// var  name = "Saad ", Age = 15, City = "Karachi";
+
+// Q#21
+// var userName ="";
+// var my_phone ="";
+// var $Cureency = "";
+// var price100 = "";
+// var Hello ="";
+
+// var 2num = "";
+// var my-phone = "";
+// var hello world = "";
+// var for = "";
+// var @saad = "";
+
+// Q#22
+// var Rules1  = ("<h1>Rules for naming JS variables</h1>");
+// var Rules2  = "For example $my_1stVariable <br>";
+// var Rules3  = "For example $name, _name or name <br>";
+// var Rules4  = "Variable names are case Sensitive <br>";
+// var Rules5  = "Variable names should not be JS Reserved Keyword <br>";
+// var result = Rules1 + Rules2 + Rules3 + Rules4 + Rules5;
+// document.write(result)
+
+// Q#23
+// var num1 = 12;
+// var num2 = 22;
+// var Add = num1 + num2;
+// document.write("This is Add of " + num1 + " and  " + num2 + " is " + Add);
+
+// var num1 = 52;
+// var num2 = 22;
+// var Sub = num1 - num2;
+// document.write("This is Sub of " + num1 + " and  " + num2 + " is " + Sub);
+
+// var num1 = 2;
+// var num2 = 12;
+// var Multiplication = num1 * num2;
+// document.write("This is Multiplication of " + num1 + " and  " + num2 + " is " + Multiplication);
+
+// var num1 = 5;
+// var num2 = 2;
+// var Divided = num1 / num2;
+// document.write("This is Divided of " + num1 + " and  " + num2 + " is " + Divided);
+
+// var num1 = 10;
+// var num2 = 6;
+// var Modules = num1 % num2;
+// document.write("This is Modules of " + num1 + " and  " + num2 + " is " + Modules);
+
+// Q#24
+// var num;
+// document.write("Value after variable declaration  " + num    + "</br>")
+// num = 5;
+// document.write("Initial value " + num   + "</br>")
+// num ++;
+// document.write("Value After Increament is:  " + num + "</br>")
+// num =num+7;
+// document.write("Value 2times Increament is:  " + num + "</br>")
+// num --;
+// document.write("Value After Decreament is:  " + num + "</br>");
+// var reminder = num%3;
+// document.write("The Reminder is: " + reminder);
+
+
+
+
